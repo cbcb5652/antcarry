ac1ec857fd39dfdeba24b59500f7a3a809446029
cbcb5652 <cbcb5652@163.com>
2021-01-28 11:56:00 +0800

    添加

---
+++ src/com/ants/programmer/dao/RedisDao.java
@@ -0,0 +1,46 @@
+package com.ants.programmer.dao;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class RedisDao {
+
+	public static Jedis getJedis() {
+		JedisPoolConfig config=new JedisPoolConfig();
+		config.setMaxTotal(30);
+		config.setMaxIdle(10);
+
+
+		Jedis  jedisCluster= new Jedis ("localhost",6379);
+//		try {
+//			Set<HostAndPort> jedisClusterNode=new HashSet<HostAndPort>();
+//			jedisClusterNode.add(new HostAndPort("127.0.0.1", 6379));
+//			jedisClusterNode.add(new HostAndPort("127.0.0.1", 6380));
+//			jedisClusterNode.add(new HostAndPort("127.0.0.1", 6381));
+//			jedisClusterNode.add(new HostAndPort("127.0.0.1", 6382));
+//			jedisClusterNode.add(new HostAndPort("127.0.0.1", 6383));
+//			jedisClusterNode.add(new HostAndPort("127.0.0.1", 6384));
+//			jedisCluster=new JedisCluster(jedisClusterNode, config);
+//
+//		}catch(Exception e) {
+//			e.printStackTrace();
+//		}
+		return jedisCluster;
+	}
+	
+	public static void closeRourse(Jedis jedis) {
+		try {
+			if (jedis != null) {
+				jedis.close();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
